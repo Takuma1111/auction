d4f5188fa9809f1d6e76c3d817a3d8e0ea74b3e1
村上拓麻 <erekguitar0120@gmail.com>
2018-03-10 15:23:03 +0900

    オークションアプリ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,5 +8,36 @@ class ItemsController < ApplicationController
         @items = Item.all
     end
 
+    def new
+        @item = Item.new  #空っぽのItem(インスタンス生成)
+    end
+
+    def create    #newとcreateはセット。出ないと登録できない
+        @item = Item.new(item_params)
+        @item.save      #.saveは保存されている
+        redirect_to "/items/#{@item.id}"
+    end
+
+    def edit
+      @item =Item.find(params[:id])
+    end
+
+    def update
+      @item = Item.find(params[:id])
+      @item.update(item_params)
+      redirect_to "/items/#{@item.id}"
+    end
+
+    def destroy
+      @item = Item.find(params[:id])
+      @item.destroy
+      redirect_to "/items"
+    end
+
+    private
+    def item_params
+      params.require(:item).permit(:name, :price, :seller_id, :description, :email, :image_url)
+    end
+
 
 end
